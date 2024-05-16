bb97dcc0bd0f9b316676dc30d564a65b45c3c305
ran an <147180833+RanAnQWQ@users.noreply.github.com>
2024-05-16 11:28:35 +0800

    update logic of isHu
    now it can judge if the tiles can Hu without the need of chi number,thing number and gang number

--- src/HuHelper/Hu.java
+++ src/HuHelper/Hu.java
@@ -11,6 +11,7 @@ public class Hu {
 
 
     private static ArrayList<Integer> getPairs(ArrayList<Integer> hand) {
+        Collections.sort(hand);
         ArrayList<Integer> res=new ArrayList<>();
         if (null != hand && hand.size() > 1) {
             for (int i = 0; i < hand.size() - 1; i++) {
@@ -26,8 +27,7 @@ public class Hu {
 
     // Method to count the number of triplets in the hand
 
-    private static int countTriplets(ArrayList<Integer> temp) {
-        int tripletCount = 0;
+    private static ArrayList<Integer> removeTriplets(ArrayList<Integer> temp) {
         Iterator<Integer> iterator = temp.iterator();
 
         while (iterator.hasNext()) {
@@ -36,7 +36,6 @@ public class Hu {
 
             if (count >= 3) {
                 int removals = count >= 4 ? 4 : 3;  // Remove 4 if there are quadruplets, otherwise remove 3
-                tripletCount += removals / 3;       // Increment by 1 for triplet, potentially by another 1 for quadruplet
                 for (int i = 0; i < removals; i++) {
                     iterator.remove();             // Remove the current element safely
                     if (i < removals - 1) {        // If not the last iteration, move to next element
@@ -48,28 +47,33 @@ public class Hu {
                 }
             }
         }
-        return tripletCount;
+        return temp;
     }
 
 
     // Method to count the number of sequences in the hand
-    private static int countSequences(ArrayList<Integer> temp) {
+    private static ArrayList<Integer> removeSequences(ArrayList<Integer> temp) {
         Collections.sort(temp);
-        int sequenceCount = 0;
         //remove Feng tiles
-        int[] feng={41,42,43,44,45,47,47};
+        int[] feng={41,42,43,44,45,46,47};
         for (Integer i:feng){
             if(temp.contains(i)){
                 temp.remove(i);
             }
         }
-        for (Integer i = 0; i < temp.size() - 2; i++) {
+        ArrayList<Integer> tempCards = (ArrayList<Integer>) temp.clone();
+        for (int i = 0; i < temp.size() - 2; i++) {
             if (temp.get(i) + 1 == temp.get(i+1)&& temp.get(i+1)+ 1 == temp.get(i+2)) {
-                sequenceCount++;
+                Integer a=temp.get(i);
+                Integer b=temp.get(i+1);
+                Integer c=temp.get(i+2);
+                tempCards.remove(a);
+                tempCards.remove(b);
+                tempCards.remove(c);
                 i += 2; // Skip the next two tiles as they're part of the sequence
             }
         }
-        return sequenceCount;
+        return tempCards;
     }
 
     // Method to count the occurrences of a specific tile in the hand
@@ -83,7 +87,7 @@ public class Hu {
         return count;
     }
 
-    public static boolean isHu(ArrayList<Integer> hand,ArrayList<Integer> HuTiles,int Chi,int Peng,int Gang){
+    public static boolean isHu(ArrayList<Integer> hand,ArrayList<Integer> HuTiles){
         if(isWinningType(hand,HuTiles)) {
             ArrayList<Integer> js = getPairs(hand);
             if (null == js || js.size() <= 0) {
@@ -94,9 +98,9 @@ public class Hu {
                     tempCards.remove(j);
                     tempCards.remove(j);
                     Collections.sort(tempCards);
-                    int triplets = countTriplets(tempCards);
-                    int sequence= countSequences(tempCards);
-                    if (triplets + sequence+ Chi+ Peng+ Gang == 4) {
+                    tempCards = removeTriplets(tempCards);
+                    tempCards= removeSequences(tempCards);
+                    if (tempCards.size() <= 0) {
                         return true;
                     }
                 }
