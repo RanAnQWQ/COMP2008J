c4aa5014fbfd30f82262a60a0260eb76918a5e9a
VictoriaZQY <163720593+VictoriaZQY@users.noreply.github.com>
2024-05-25 22:46:24 +0800

    modified
    This was then deleted. The Game class is then moved  and modified to aGame() method in the GameContent class in the window package, but this is the  draft of the whole game process.

--- src/GameTable/Game.java
+++ src/GameTable/Game.java
@@ -1,124 +1,115 @@
 package GameTable;
 
 import Player.*;
+import tiles.Tilemap;
+import window.*;
+import HuHelper.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
  * This is the whole game process.
  *
- * To start with, we deal the cards (the number of cards are different between the boss and normal ones)
+ * To start with, we deal the cards (the number of cards are different between the host and normal ones)
  * After that, the player and computers will gain and discard their cards in turns.
  * During the process, the chance to Chi, Peng, Gang, Ting, Hu will pop up as a button.
  * The player can choose to take the action or not, meanwhile, the computers will take that action automatically.
  *
+ * the Game class is then moved and modified to
+ * aGame() method in the GameContent class in the window package,
+ * but this is the whole process is mapped here initially.
+ *
+ * This has no use anymore except as a draft of the whole game process.
+ *
  * @author Qiyue Zhu
  */
+
 public class Game {
+    //store the information of each in the players
+    public static List<Player> players=new ArrayList<Player>();
+
     public static void main(String[] args) throws InterruptedException {
+        GameWindow gameWindow = new GameWindow();  // initial the game window
+
         // initial the players
-        InitPlayer initPlayer = null;
+        InitPlayer initPlayer = new InitPlayer();
 
         // create 4 players, and 3 of them are computers
-        Player player = null;
-        Computer computer1 = null, computer2 = null, computer3 = null;
+        Player player = initPlayer.player;
+        Computer computer1 = initPlayer.computer1, computer2 = initPlayer.computer2, computer3 = initPlayer.computer3;
 
         // create and shuffle the Majiang cards
-        ShuffleMajiang shuffleMajiang = null;
+        ShuffleMajiang shuffleMajiang = new ShuffleMajiang();
 
 
-        // create and shuffle the cards
-        shuffleMajiang=new ShuffleMajiang();
-
-        // throw the dice
-        //GameWindow win = new GameWindow();
-        //int diceNum= win.sum;
-
-        // decide the boss
-
-
-        // create players
-        initPlayer = new InitPlayer();
-        // deal the cards the first time evenly
+        // deal the cards for the first time evenly (13 cards each)
         initPlayer.haveFirstBoard();
 
 
-        // print the players' cards
-        initPlayer.printPlayer();
-        //输出牌局中的整副牌（剩余的牌）
-        System.out.println("【此时牌局中的剩余的牌：】");
-        shuffleMajiang.printMaJiangs(0);
+        // sort the cards
+        Collections.sort(player.getPlayerMajiangs());
+        Collections.sort(computer1.getPlayerMajiangs());
+        Collections.sort(computer2.getPlayerMajiangs());
+        Collections.sort(computer3.getPlayerMajiangs());
+
+        // in the first turn, the host will gain one more card to discard first
+        initPlayer.players.get(0).gainMajiang(1);  // index are got from the window
 
 
-        // add one more card to the boss
-        initPlayer.haveJumpBoard();
+        System.out.println("player: " + player.playerMajiangs);
+        System.out.println("size: " + player.playerMajiangs.size());
 
 
-        //输出此时玩家双手中的牌
-        initPlayer.printPlayer();
-        //输出牌局中的整副牌（剩余的牌）
-        System.out.println("【此时牌局中的剩余的牌：】");
-        shuffleMajiang.printMaJiangs(0);
+        gameWindow.tilemap = new Tilemap();
+        gameWindow.setTileNumber(player.getPlayerMajiangs());
+        System.out.println("tile: " + gameWindow.tileNumber);
+
+        gameWindow.addTileToWindow();
+        gameWindow.hideTiles();
 
 
-        // change this according to the listener
-        System.out.println("【 您是庄家，请打一张牌。 请输入:[待打出牌对应的顺序，例如：1表示打出第一张牌] 】");
-        //String inputdiscardBoard = scanner.nextLine();
-
-        player=initPlayer.players.get(0);
-        computer1=(Computer) initPlayer.players.get(1);
-        computer2=(Computer) initPlayer.players.get(2);
-        computer3=(Computer) initPlayer.players.get(3);
+        // in the first turn, the host will discard a card
+        System.out.println(gameWindow.cardToDiscard);
+        //initPlayer.players.get(0).discardMajiang(player.playerMajiangs.indexOf(gameWindow.cardToDiscard));  // index are got from the window
 
 
-        // discard a card
-        //player.discardMajiang(Integer.valueOf(inputdiscardBoard)-1);
+        // to judge if any of the players will Hu or not
+        //boolean playerHu = Hu.isHu(player.getPlayerMajiangs(), player.getCardsToDisplay(), player.ChiNumber, player.PengNumber, player.GangNumber);
+        //boolean computer1Hu = Hu.isHu(computer1.getPlayerMajiangs(), computer1.getCardsToDisplay(), computer1.ChiNumber, computer1.PengNumber, computer1.GangNumber);
+        //boolean computer2Hu = Hu.isHu(computer2.getPlayerMajiangs(), computer2.getCardsToDisplay(), computer2.ChiNumber, computer2.PengNumber, computer2.GangNumber);
+        //boolean computer3Hu = Hu.isHu(computer3.getPlayerMajiangs(), computer3.getCardsToDisplay(), computer3.ChiNumber, computer3.PengNumber, computer3.GangNumber);
 
 
-        //输出此时玩家双手中的牌
-        System.out.println("【此时玩家手中的牌：】");
-        player.printMaJiangs();
-        //输出河里的牌
-        System.out.println("\n【河里牌：】");
-        shuffleMajiang.printMaJiangsRiver();
+        if (!ShuffleMajiang.river.isEmpty()) {
+            // take turns to play, always start with the host
+            for (int turn = 1; turn < 4; turn++) {
+                // if any of the players Hu, the game will end
+                //if ((!playerHu) && (!computer1Hu) && (!computer2Hu) && (!computer3Hu)) {
 
 
+                //and for others, the possible movements like Peng, Gang will be detected
+                Movement movement2 = new Movement(initPlayer.players.get((turn + 1) % 4).getPlayerMajiangs(), initPlayer.players.get((turn + 1) % 4).ChiNumber, initPlayer.players.get((turn + 1) % 4).PengNumber, initPlayer.players.get((turn + 1) % 4).GangNumber);
+                Movement movement3 = new Movement(initPlayer.players.get((turn + 2) % 4).getPlayerMajiangs(), initPlayer.players.get((turn + 2) % 4).ChiNumber, initPlayer.players.get((turn + 2) % 4).PengNumber, initPlayer.players.get((turn + 2) % 4).GangNumber);
+                Movement movement4 = new Movement(initPlayer.players.get((turn + 3) % 4).getPlayerMajiangs(), initPlayer.players.get((turn + 3) % 4).ChiNumber, initPlayer.players.get((turn + 3) % 4).PengNumber, initPlayer.players.get((turn + 3) % 4).GangNumber);
 
 
+                // in one's turn, the movement of Chi, Peng, Gang will happen
+                Movement movement1 = new Movement(player.playerMajiangs, ShuffleMajiang.riverLastCard, initPlayer.players.get(turn).ChiNumber, initPlayer.players.get(turn).PengNumber, initPlayer.players.get(turn).GangNumber);
 
-        while (true) {
-            // computer turn
-            computer1.gainMajiang(0);
-            // computer discard a card
-            System.out.println("【电脑打出了："+computer1.discardMajiang(1)+"】");
+                // in one's turn, the player will gain a card and discard one
+                initPlayer.players.get(turn).gainMajiang(1);  // index are got from the window
+                //initPlayer.players.get(turn).discardMajiang(player.playerMajiangs.indexOf(gameWindow.cardToDiscard));  // index are got from the window
 
-            // player's turn
-            System.out.println("【开始打牌：请输入:[1:起牌]，[2:打牌]，[3:碰牌]，[4:杠牌]，[5:胡牌]，[6:不做操作]，[其他输入:退出游戏]】");
-            String inputOperate = "1";
-            if (shuffleMajiang.maJiangs.size()==0) {
-                System.out.println("【留局】");
-                System.out.println("【游戏结束，再见！】");
-                System.exit(0);
-            }
-            if ("1".equals(inputOperate)) {
-                player.gainMajiang(0);
-            }else if("2".equals(inputOperate)) {
-                player.discardMajiang(0);
-            }else if("3".equals(inputOperate)) {
-                // 碰
-                Movement peng = new Movement(player.getPlayerMajiangs());
-                peng.Peng();
-            }else if("4".equals(inputOperate)) {
-                // 杠
-                Movement gang = new Movement(player.getPlayerMajiangs());
-                gang.Gang();
-            }else if("6".equals(inputOperate)){
-                //不做处理，继续下一次
-                continue;
-            }else {
-                System.out.println("【游戏结束，再见！】");
-                System.exit(0);
+                /*
+        // after a Peng or Gang happens, the order of turn will change
+        if ( this.PengNumber != PengNumber || this.GangNumber != GangNumber ){
+             currentPlayer = ;
+        }*/
             }
         }
+        //}
     }
-
 }
--- src/GameTable/ShuffleMajiang.java
+++ src/GameTable/ShuffleMajiang.java
@@ -4,9 +4,11 @@ package GameTable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 /**
  * ShuffleMajiang: this class aims to create and shuffle the cards
+ * @author: Qiyue Zhu
  */
 public class ShuffleMajiang {
     // store all the cards
@@ -16,26 +18,48 @@ public class ShuffleMajiang {
     public static int maJiangsIndex=0;
 
     // river: the cards been discarded
-    public static List<Integer> river=new ArrayList<Integer>();
+    public static ArrayList<Integer> river=new ArrayList<Integer>();
 
     // current index of the cards in the river
     public static int riverIndex=0;
 
 
+
+    // this method is written by Ran An
+    public static ArrayList<Integer> getRiver() {
+        if(!river.isEmpty()){
+            return river;
+        }else{
+            return null;
+        }
+    }
+
+
+    /**
+     * ShuffleMajiang(): create and shuffle cards
+     * @throws InterruptedException
+     */
     public ShuffleMajiang() throws InterruptedException {
-        //create majiangs
-        maJiangs = new ArrayList<>(135);
-        for (int i = 11; i < 47; i++){
-            for (int j = 1; j <= 4; j++){
+        Random random = new Random();
+        //create majiangs and add them into maJiangs
+        maJiangs = new ArrayList<>(132);
+        for (int i = 11; i < 47; i++) {
+            // 4 packs of Majiangs
+            for ( int j = 1; j <= 4; j++) {
+                // don't have the cards of 20, 30, 40
+                if (i == 20 || i == 30 || i == 40){
+                    break;
+                }
+                // add the card to the maJiangs
                 maJiangs.add(i);
             }
         }
-
         // shuffle the cards that have been created in order
         Collections.shuffle(maJiangs);
-
+        System.out.println(maJiangs.size());
         // restart teh indexes
         riverIndex=0;
         maJiangsIndex=135;
     }
+
 }
